95648532db12c63f12108b85f4e71459b0a3b7c1
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-07-28 15:39:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make= make;
+	this.model = model;
+	getMakeModel(){
+		return (this.make, this.model)
+	}
+	
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.__proto__ = SportsCar;
+
+function SportsCar(make, model, topSpeed) {
+	SportsCar.call(Car,make, model)
+	
+	getTopSpeed(topSpeed){
+		this.topSpeed = topSpeed;	
+	}
+	
+}
 
 // Do not change the code below
 window.Car = Car;
