56465d62abcd1d7631a7e651319748184ebc6462
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-07-28 16:02:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-// Complete the js code
+// write your code
 function Car(make, model) {
-	this.make= make;
-	this.model = model;
-	getMakeModel(){
-		return (this.make, this.model)
-	}
-	
+    this.make = make;
+    this.model = model;
 }
 
-Car.__proto__ = SportsCar;
+// Add a prototype method to Car
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
 
+// Define the SportsCar constructor function
 function SportsCar(make, model, topSpeed) {
-	SportsCar.call(Car,make, model)
-	
-	getTopSpeed(topSpeed){
-		this.topSpeed = topSpeed;	
-	}
-	
+    // Call the Car constructor with the current instance
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
+
+// Set up inheritance so SportsCar inherits from Car
+SportsCar.prototype = Object.create(Car.prototype);
+// Restore the constructor property on SportsCar.prototype
+SportsCar.prototype.constructor = SportsCar;
+
+// Add a prototype method to SportsCar
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
